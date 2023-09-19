59cd5458f4b06453e36fa49b7f40de7a133f7aec
Charley <tianjie397525099@126.com>
2023-09-19 17:37:49 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="测试gitPage" />
+      <HelloWorld msg="测试gitPage,测试" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
